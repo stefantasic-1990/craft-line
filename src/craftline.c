adaf4a0730cb74fe446c59ed07af345e02800585
Stefan Tasic <stefantasic.2790@gmail.com>
2025-10-25 12:32:19 -0400

    move line redraw to line.c

--- src/craftline.c
+++ src/craftline.c
@@ -7,20 +7,6 @@
 #include <unistd.h>
 #include <errno.h>
 
-static void redraw_line(char *prompt, int prompt_len, line_state line)
-{
-    // repaint: move to col 0, print prompt + visible slice of line buffer, clear to end of window (removes leftover chars if needed).
-    write(STDOUT_FILENO, "\x1b[0G", 4);
-    write(STDOUT_FILENO, prompt, prompt_len);
-    write(STDOUT_FILENO, line.line_buffer + line.line_display_offset, line.curr_line_len < line.line_display_len ? line.curr_line_len : line.line_display_len);
-    write(STDOUT_FILENO, "\x1b[0K", 4);
-
-    // place cursor after prompt at logical position.
-    char cursor_esc_code[10];
-    snprintf(cursor_esc_code, sizeof(cursor_esc_code), "\x1b[%iG", prompt_len + 1 + line.curr_cursor_pos - line.line_display_offset);
-    write(STDOUT_FILENO, cursor_esc_code, strlen(cursor_esc_code));
-}
-
 static int read_escseq(char escseq[2]) {
     for (int i = 0; i < 2; i++) {
         ssize_t n;
--- src/line.h
+++ src/line.h
@@ -3,11 +3,13 @@
 
 typedef struct {
     char *line_buffer;
-    int line_buffer_size;
+    int line_buffer_size;     // current size of the line buffer
     int curr_line_len;        // current line length
     int curr_cursor_pos;      // current cursor position within line buffer.
     int line_display_offset;  // horizontal scroll offset; line is printed on screen starting here.
     int line_display_len;     // visible chars available on screen after prompt is printed; depends on window size.
 } line_state;
+
+void redraw_line(char *prompt, int prompt_len, line_state line);
 
 #endif
